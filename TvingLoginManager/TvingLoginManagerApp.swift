32ba06f21e9be1893bc22e89f9b5d36a61353a49
김도연 <doyeon.kim35@tving.com>
2026-06-08 16:07:36 +0900

    chore: scaffold Swift macOS project with Keychain and sandbox entitlements

---
+++ TvingLoginManager/TvingLoginManagerApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct TvingLoginManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("TVING Login Manager - Setup Complete")
+                .frame(width: 400, height: 300)
+        }
+    }
+}
---
+++ TvingLoginManagerTests/TvingLoginManagerTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+final class TvingLoginManagerTests: XCTestCase {
+    func testPlaceholder() {
+        XCTAssertTrue(true)
+    }
+}
