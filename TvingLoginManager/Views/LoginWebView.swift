828909ca3dd573f29f69c04575593abc0c2222ff
김도연 <doyeon.kim35@tving.com>
2026-06-09 15:04:03 +0900

    chore: remove debug network monitoring scripts from LoginWebView

--- TvingLoginManager/Views/LoginWebView.swift
+++ TvingLoginManager/Views/LoginWebView.swift
@@ -61,48 +61,6 @@ struct WebViewContainer: NSViewRepresentable {
         let config = WKWebViewConfiguration()
         config.websiteDataStore = .nonPersistent()
 
-        // 네트워크 요청 모니터링 스크립트 주입
-        let monitorScript = WKUserScript(source: """
-            (function() {
-                // fetch 가로채기
-                var origFetch = window.fetch;
-                window.fetch = function(url, opts) {
-                    var method = (opts && opts.method) || 'GET';
-                    var body = (opts && opts.body) || '';
-                    console.log('[NETWORK] ' + method + ' ' + url);
-                    if (body) console.log('[BODY] ' + (typeof body === 'string' ? body : JSON.stringify(body)));
-                    return origFetch.apply(this, arguments);
-                };
-
-                // XMLHttpRequest 가로채기
-                var origOpen = XMLHttpRequest.prototype.open;
-                var origSend = XMLHttpRequest.prototype.send;
-                XMLHttpRequest.prototype.open = function(method, url) {
-                    this._method = method;
-                    this._url = url;
-                    return origOpen.apply(this, arguments);
-                };
-                XMLHttpRequest.prototype.send = function(body) {
-                    console.log('[NETWORK] ' + this._method + ' ' + this._url);
-                    if (body) console.log('[BODY] ' + body);
-                    return origSend.apply(this, arguments);
-                };
-            })();
-            """, injectionTime: .atDocumentStart, forMainFrameOnly: false)
-        config.userContentController.addUserScript(monitorScript)
-
-        // console.log를 Swift에서 수신
-        config.userContentController.add(context.coordinator, name: "logHandler")
-        let consoleScript = WKUserScript(source: """
-            var origLog = console.log;
-            console.log = function() {
-                var msg = Array.from(arguments).join(' ');
-                origLog.apply(console, arguments);
-                window.webkit.messageHandlers.logHandler.postMessage(msg);
-            };
-            """, injectionTime: .atDocumentStart, forMainFrameOnly: false)
-        config.userContentController.addUserScript(consoleScript)
-
         let webView = WKWebView(frame: .zero, configuration: config)
         webView.navigationDelegate = context.coordinator
         context.coordinator.loginService.configure(
@@ -119,13 +77,7 @@ struct WebViewContainer: NSViewRepresentable {
 
     func updateNSView(_ nsView: WKWebView, context: Context) {}
 
-    class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
-        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-            if let body = message.body as? String {
-                print("🌐 \(body)")
-            }
-        }
-
+    class Coordinator: NSObject, WKNavigationDelegate {
         let loginService = LoginService()
         private var hasStartedLogin = false
         private let onComplete: (Bool, String) -> Void
